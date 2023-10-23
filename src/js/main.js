925a8f11947feb7b3fe35d5aa09180e887bb2805
Yana <nanaha28@gmail.com>
2023-10-24 02:05:48 +0300

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,27 @@
+// 1. Разбить сайт на блоки
+// -header-navigation
+// -header 
+
+// -special-products
+
+// -info-block
+
+// -product-collections
+//     - header-product-collection
+//     -selection
+//     -collection-items
+
+// -feedback-block
+// -contact-block 
+//  -footer 
+
+//  -product-cart
+
+//  -modul
+
+
+
+// 2. Выделить общие элементы
+
+
+// 3. Выделить общие цвета
